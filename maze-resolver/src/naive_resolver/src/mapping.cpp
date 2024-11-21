b5f7c927ed07c4af900ed88b00778ea5ee3b046f
ItsVasconcellos <fernando.asv@uol.com.br>
2024-11-21 10:58:38 -0300

    feat: adding map to class

--- maze-resolver/src/naive_resolver/src/mapping.cpp
+++ maze-resolver/src/naive_resolver/src/mapping.cpp
@@ -10,10 +10,11 @@
 class MazeClient : public rclcpp::Node
 {
 public:
-    
+    std::vector<std::vector<uint16_t>> map;
+    std::vector<uint8_t> map_size;
+
     MazeClient() : Node("maze_client")
     {
-
         client_move_cmd_ = this->create_client<cg_interfaces::srv::MoveCmd>("/move_command");
         client_get_map_ = this->create_client<cg_interfaces::srv::GetMap>("get_map");
 
@@ -61,13 +62,13 @@ public:
         auto request = std::make_shared<cg_interfaces::srv::GetMap::Request>();
 
         auto result = client_get_map_->async_send_request(request,
-                                                          [](rclcpp::Client<cg_interfaces::srv::GetMap>::SharedFuture future)
+                                                          [this](rclcpp::Client<cg_interfaces::srv::GetMap>::SharedFuture future)
                                                           {
                                                               try
                                                               {
                                                                   auto response = future.get();
-                                                                  RCLCPP_INFO(rclcpp::get_logger("MazeClient"), "Mapa recebido: ");
-                                                                  // Aqui você pode processar o mapa recebido
+                                                                  RCLCPP_INFO(rclcpp::get_logger("MazeClient"), "Map recieved, it has size: %d x %d", response->occupancy_grid_shape[0], response->occupancy_grid_shape[1]);
+                                                                  setMap(response->occupancy_grid_shape[0], response->occupancy_grid_shape[1], response->occupancy_grid_flattened);
                                                               }
                                                               catch (const std::exception &e)
                                                               {
@@ -76,10 +77,50 @@ public:
                                                           });
     }
 
+    void setMap(uint8_t height, uint8_t width, std::vector<std::string> occupancy_grid_flattened)
+    {
+        this->map_size = {height, width};
+        this->map = std::vector<std::vector<uint16_t>>(height, std::vector<uint16_t>(width, 0));
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (occupancy_grid_flattened[i * width + j] == "b")
+                {
+                    this->map[i][j] = 0;
+                }
+                if (occupancy_grid_flattened[i * width + j] == "f")
+                {
+                    this->map[i][j] = 1;
+                }
+                if (occupancy_grid_flattened[i * width + j] == "t")
+                {
+                    this->map[i][j] = 2;
+                }
+                if (occupancy_grid_flattened[i * width + j] == "r")
+                {
+                    this->map[i][j] = 3;
+                }
+                RCLCPP_INFO(rclcpp::get_logger("MazeClient"), "Map square: %d", this->map[i][j]);
+            }
+        }
+        RCLCPP_INFO(rclcpp::get_logger("MazeClient"), "Map processed!");
+    }
+
+    void makePath()
+    {
+    }
+
+    void followPath()
+    {
+        
+    }
+
 private:
     rclcpp::Client<cg_interfaces::srv::MoveCmd>::SharedPtr client_move_cmd_;
     rclcpp::Client<cg_interfaces::srv::GetMap>::SharedPtr client_get_map_;
 };
+;
 
 int main(int argc, char **argv)
 {
@@ -88,6 +129,8 @@ int main(int argc, char **argv)
     auto client = std::make_shared<MazeClient>();
 
     client->requestMap();
+    client->makePath();
+    client->followPath();
 
     rclcpp::spin(client);
     rclcpp::shutdown();
